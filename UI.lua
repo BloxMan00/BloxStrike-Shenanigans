b92bf3b42cb02416b787c64fa1e3e91aec2553dd
BloxMan00 <112082580+BloxMan00@users.noreply.github.com>
2026-04-29 12:02:12 +0200

    Update UI.lua

--- UI.lua
+++ UI.lua
@@ -2884,7 +2884,9 @@ end
 function library:Toggle()
     if not self.opening then
         self.opening = true
-        self.Playerlist.object.Visible = self.Playerlist.toggled and not self.open or false
+        if self.Playerlist then
+            self.Playerlist.object.Visible = self.Playerlist.toggled and not self.open or false
+        end
 
         for window, types in next, library.windows do
             window.Visible = types.Visible and not self.open or false;
@@ -3851,8 +3853,8 @@ function library:Playerlist(max_players)
             player_data[plr].name = plr.Name
 
             spawn(function()
-                local thumbnail_data = services.HttpService:JSONDecode(syn.request{Url = ("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%s&size=60x60&format=Png"):format(plr.UserId), Method = "GET"}.Body)
-                local image = syn.request{Url = thumbnail_data.data[1].imageUrl, Method = "GET"}.Body
+                local thumbnail_data = services.HttpService:JSONDecode(request{Url = ("https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds=%s&size=60x60&format=Png"):format(plr.UserId), Method = "GET"}.Body)
+                local image = request{Url = thumbnail_data.data[1].imageUrl, Method = "GET"}.Body
 
                 player_data[plr].image = image
                 
@@ -4101,6 +4103,11 @@ function library:Playerlist(max_players)
 
     function library.Playerlist:IsTagged(player, tag)
         return player_data[player].tags[tag]
+    end
+
+    function library.Playerlist:Toggle()
+        self.toggled = not self.toggled
+        self.object.Visible = self.toggled
     end
 
     utility.format(library.Playerlist, true)
